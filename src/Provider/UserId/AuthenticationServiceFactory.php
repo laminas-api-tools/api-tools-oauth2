596418329367aceb8d7d7eb2c2bd3014ead0d748
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:12:36 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Provider/UserId/AuthenticationServiceFactory.php
+++ src/Provider/UserId/AuthenticationServiceFactory.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-oauth2 for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-oauth2/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-oauth2/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ApiTools\OAuth2\Provider\UserId;
 
 use Interop\Container\ContainerInterface;
